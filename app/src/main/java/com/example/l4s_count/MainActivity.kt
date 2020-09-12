0171e9f553a1e6ebab82ccaaa2ba195503813fa8
numabesachi <numabesachi@gmail.com>
2020-09-12 14:25:47 +0900

    finish_fizzbuzz

--- app/src/main/java/com/example/l4s_count/MainActivity.kt
+++ app/src/main/java/com/example/l4s_count/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.l4s_count
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,18 +12,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var number: Int = 0
         numberTextView.text ="0"
+        fizzBuzz(number)
 
         plusButton.setOnClickListener {
             number += 1
             numberTextView.text = number.toString()
+            fizzBuzz(number)
         }
         minusButton.setOnClickListener {
             number -= 1
             numberTextView.text = number.toString()
+            fizzBuzz(number)
         }
         resetButton.setOnClickListener {
             number = 0
             numberTextView.text = number.toString()
+            fizzBuzz(number)
         }
     }
+
+    fun fizzBuzz(i:Int){
+        when {
+            i % 15 == 0 -> numberTextView.backgroundTintList = ColorStateList.valueOf(Color.rgb(250,0,250))
+            i % 3 == 0 -> numberTextView.backgroundTintList = ColorStateList.valueOf(Color.rgb(250,0,0))
+            i % 5 == 0 -> numberTextView.backgroundTintList = ColorStateList.valueOf(Color.rgb(0,0,250))
+            else -> numberTextView.backgroundTintList = ColorStateList.valueOf(Color.rgb(250,250,250))
+            }
+    }
+
+
 }
